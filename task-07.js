5abf208e059b82903693f28e83ab18d99a9352d5
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-18 00:25:18 +0600

    Task 07

---
+++ task-07.js
@@ -0,0 +1,9 @@
+// Take a speed variable. If speed is greater than 80, print "Over speeding", otherwise print "Normal speed".
+
+let speed = 90;
+
+if (speed > 80) {
+  console.log("Over speeding");
+} else {
+  console.log("Normal speed");
+}
