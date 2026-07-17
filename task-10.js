2cc406d44ad47ad79e83578a93cc993b562f332e
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-18 01:15:51 +0600

    Task 10

---
+++ task-10.js
@@ -0,0 +1,10 @@
+// Build a simple login system — print "Login successful" only if both username and password are correct.
+
+const username = "admin";
+const password = "1234";
+
+if (username === "admin" && password === "1234") {
+  console.log("Login successful");
+} else {
+  console.log("Username and not matched");
+}
