7b18c8f9bd87e037030983a38f6e3770409a1ec1
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-18 00:55:29 +0600

    Task 09

---
+++ task-09.js
@@ -0,0 +1,10 @@
+// Using isWeekend and isHoliday, print "No work today" if either one is true (use ||).
+
+let isWeekend = true;
+let isHoliday = false;
+
+if (isHoliday || isWeekend) {
+  console.log("No work today");
+} else {
+  console.log("No rest today.");
+}
