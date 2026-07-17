95b4784965419cdb457d29a53b4ea6aff23aa327
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-18 00:32:42 +0600

    Task 08

---
+++ task-08.js
@@ -0,0 +1,9 @@
+// Using age and hasTicket, print "Entry allowed" if age is above 18 AND the person has a ticket (use &&).
+
+let age = 17;
+let hasTicket = true;
+if (age >= 18 && hasTicket) {
+  console.log("Entry allowed");
+} else {
+  console.log("You are not fucking allowed");
+}
