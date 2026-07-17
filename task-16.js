53564ae00449130060b44a1772f8c49c40ea17a0
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-18 03:44:58 +0600

    Task 16

---
+++ task-16.js
@@ -0,0 +1,6 @@
+//  Determine whether a number is even or odd using the ternary operator.
+
+let number = 32;
+number % 2 === 0
+  ? console.log(number + " is an even number")
+  : console.log(number + " is a odd number");
