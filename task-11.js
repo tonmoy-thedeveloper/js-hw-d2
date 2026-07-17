c927d03265967714706ddf32852732560ac7b513
tonmoy-thedeveloper <tonmoy.thedeveloper@gmail.com>
2026-07-18 01:25:21 +0600

    Task 11

---
+++ task-11.js
@@ -0,0 +1,15 @@
+// Build a grading system using a marks variable (A+, A, B, C, F) — it must have at least 5 condition branches.
+
+let marks = 44;
+
+if (marks >= 80) {
+  console.log("A+");
+} else if (marks >= 70) {
+  console.log("B");
+} else if (marks >= 60) {
+  console.log("C");
+} else if (marks >= 50) {
+  console.log("D");
+} else {
+  console.log("F");
+}
